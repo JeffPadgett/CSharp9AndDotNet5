70d360a92c66597389171133966cb7e92182de9c
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-02-19 17:32:16 -0500

    Tracing and Debug C# classes for error logging

---
+++ Debugging/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Debugging
+{
+    class Program
+    {
+        static double Add(double a, double b)
+        {
+            return a + b ;//deliberate bug
+        }
+        static void Main(string[] args)
+        {
+            double a = 4.5;
+            double b = 2.5;
+            double answer = Add(a, b);
+            WriteLine($"{a} + {b} = {answer}");
+            ReadLine(); //Wait for user to press Enter
+        }
+    }
+}
---
+++ Instrumenting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Instrumenting
+{
+    class Program
+    {
+        static void Main()
+        {
+            // write to a text file to the project folder
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+
+            //text writer is buffered, so this option calls Flush() on all listeners after writing
+            Trace.AutoFlush = true;
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(displayName: "PacketSwitch", description: "This switch is set via a JSON config.");
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace Error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace Information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
+
+        }
+    }
+}
