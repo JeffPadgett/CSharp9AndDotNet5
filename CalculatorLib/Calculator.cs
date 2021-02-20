42fcc837cb13684481b4f65273585f48072335eb
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-02-19 19:09:44 -0500

    unit test example

---
+++ CalculatorLib/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
