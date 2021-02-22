04ca35c3801cad203401aaf1db9225e4304ec1e7
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-02-22 15:43:02 -0500

    Packt class lib

---
+++ Chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        
+    }
+}
---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
