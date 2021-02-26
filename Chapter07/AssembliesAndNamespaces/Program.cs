d74055094e96c0a8c932b752ee246689968b8c70
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-02-26 09:59:41 -0500

    .net standard class library init

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XDocument();
+
+            string s1 = "hello";
+            String s2 = "World";
+            WriteLine($"{s1} {s2}");
+            Int32 testgo = 32;
+        }
+    }
+}
