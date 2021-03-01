0cca8786b1423acc4c09783e8c4e448ee545fa62
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-03-01 16:34:58 -0500

    Working with images with ImageSharp

---
+++ Chapter08/WorkingWithImages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
