0bc53bac77a5fa7a8350bfb89f9f567327baa44a
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-03-02 11:55:20 -0500

    Working with images

--- Chapter08/WorkingWithImages/Program.cs
+++ Chapter08/WorkingWithImages/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace WorkingWithImages
 {
@@ -6,7 +10,20 @@ namespace WorkingWithImages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string imagesFolder = Path.Combine(Environment.CurrentDirectory, "Images");
+
+            IEnumerable<string> images = Directory.EnumerateFiles(imagesFolder);
+
+            foreach (string imagePath in images)
+            {
+                string thumbnailpath = Path.Combine(Environment.CurrentDirectory, "Images", Path.GetFileNameWithoutExtension(imagePath) + "-thumbnail" + Path.GetExtension(imagePath));
+                using (Image image = Image.Load(imagePath))
+                {
+                    image.Mutate(x => x.Resize(image.Width / 10, image.Height / 10));
+                    image.Save(thumbnailpath);
+                }
+            }
+            Console.WriteLine("Complete");
         }
     }
 }
