7b4edc5d6e06e4ab4b85a8184e49cff95f4f3602
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-03-02 13:27:24 -0500

    created console app that test regular Expressions

---
+++ Chapter08/RegExTester/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+
+namespace RegExTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //init var
+            bool continuationFlag = true;
+            do
+            {
+                // if (escNotPressed == true)
+                // {
+                //     return;
+                // }
+
+                Write("Please enter a regular expression (or press ENTER to use the default): ");
+                Regex regExpression = new Regex(ReadLine());
+                if (regExpression.ToString() == "")
+                {
+                    regExpression = new Regex(@"\d");
+                }
+                WriteLine("Now enter some input to see if it matches:");
+                string userInput = ReadLine();
+
+                if (regExpression.IsMatch(userInput))
+                {
+                    WriteLine($"True - {regExpression.ToString()} matches {userInput}");
+                }
+                else
+                {
+                    WriteLine("False, does not match.");
+                }
+
+                WriteLine("Press anykey to try again or press esc to exit.");
+                var key = Console.ReadKey();
+                if (key.Key.ToString().ToLower() == "escape" )
+                {
+                    continuationFlag = false;
+                    WriteLine("Esc was pressed");
+                }
+
+            } while(continuationFlag);
+            
+        }
+    }
+}
