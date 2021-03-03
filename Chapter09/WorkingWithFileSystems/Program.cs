e0af8ebecc2bfdc92467a012c584e3ee5fa6f4b1
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-03-03 14:46:51 -0500

    working with files

--- Chapter09/WorkingWithFileSystems/Program.cs
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -68,11 +68,55 @@ namespace WorkingWithFileSystems
             WriteLine($"Does it exist? {Exists(newFolder)}");
             
         }
+
+        static void WorkingWithFiles()
+        {
+            // Define a directory path to output files
+            // starting in the user's folder
+
+            var dir = Combine(GetFolderPath(SpecialFolder.Personal), "Code", "Chapter09", "OutputFiles");
+
+            CreateDirectory(dir);
+
+            // define file paths
+            string textFile = Combine(dir, "Dummy.txt");
+            string backupFile = Combine(dir, "Dummy.bak");
+            WriteLine($"Working with: {textFile}");
+
+            // check if a file exist
+            WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+            // create a new text file and write a line to it
+            StreamWriter textWriter = File.CreateText(textFile);
+            textWriter.WriteLine("Hello, C#!");
+            textWriter.Close();// close file and release resources
+            WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+            //copy the file, and overwrite if it already exist
+            File.Copy(sourceFileName: textFile, destFileName: backupFile, overwrite: true);
+            WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+            Write("Confirm the files exist, and then press ENTER:");
+            ReadLine();
+
+            // delete file
+            File.Delete(textFile);
+            WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+            //read from the text file backup
+            WriteLine($"Reading contents of {backupFile}:");
+            StreamReader textReader = File.OpenText(backupFile);
+            WriteLine(textReader.ReadToEnd());
+            textReader.Close();
+
+
+
+        }
         static void Main(string[] args)
         {
             //OutputFileSystemInfo();
             //WorkWithDrives();
-            WorkingWithDirectories();
+            //WorkingWithDirectories();
+            WorkingWithFiles();
         }
     }
 }
