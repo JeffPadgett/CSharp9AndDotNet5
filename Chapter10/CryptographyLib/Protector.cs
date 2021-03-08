c960e7517eb70e860a3fb7bfcd192080024147db
Jeffrey Padgett <jeff@Jeffreys-MBP-2.attlocal.net>
2021-03-08 16:10:38 -0500

    Siging data for security

--- Chapter10/CryptographyLib/Protector.cs
+++ Chapter10/CryptographyLib/Protector.cs
@@ -1,106 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text;
 using System.Xml.Linq;
 using static System.Convert;
+using static System.Console;
+
 
 namespace Packt.Shared
 {
-    public class Protector
+
+    public class User
     {
-        private static Dictionary<string, User> Users = new Dictionary<string, User>();
-        public static User Register(string username, string password)
-        {
-            //generate a random salt
-            var rng = RandomNumberGenerator.Create();
-            var saltBytes = new byte[16];
-            rng.GetBytes(saltBytes);
-            var saltText = Convert.ToBase64String(saltBytes);
+        public string Name { get; set; }
+        public string Salt { get; set; }
+        public string SaltedHashedPassword { get; set; }
+        public string[] Roles { get; set; }
 
-            // generate the salted and hashed password
-            var saltedHashedPassword = SaltAndHashPassword(password, saltText);
-
-            var user = new User 
-            {
-                Name = username,
-                Salt = saltText,
-                SaltedHashedPassword = saltedHashedPassword
-            };
-            Users.Add(user.Name, user);
-            return user;
-        }
-
-        public static bool CheckPassword(string username, string password)
-        {
-            if (!Users.ContainsKey(username))
-            {
-                return false;   
-            }
-            var user = Users[username];
-
-            //re-generate the salted and hashed password
-            var saltedHashedPassword = SaltAndHashPassword(password, user.Salt);
-
-            return (saltedHashedPassword == user.SaltedHashedPassword);
-        }
-
-        private static string SaltAndHashPassword(string password, string salt)
-        {
-            var sha = SHA256.Create();
-            var saltedPassword = password + salt;
-            return Convert.ToBase64String(sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
-        }
-
+    }
+    public static class Protector
+    {
         // salt size must be at least 8 bytes, we will use 16 bytes
-        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+        private static readonly byte[] salt =
+          Encoding.Unicode.GetBytes("7BANANAS");
 
-        // iterations must be at least 1000, we will use 2000
-        private static readonly int iterations = 2000;
+        // iterations should be high enough to take at least 100ms to 
+        // generate a Key and IV on the target machine. 50,000 iterations
+        // takes 131ms on my 3.3 GHz Dual-Core Intel Core i7 MacBook Pro.
+        private static readonly int iterations = 50_000;
 
-
-        public static string Encrypt(string plainText, string password)
+        public static string Encrypt(
+          string plainText, string password)
         {
             byte[] encryptedBytes;
             byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
 
             var aes = Aes.Create(); // abstract class factory method
 
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations); //pbkdf stands for Password-Based key Derivation Function 2.
-            //Makes it harder for someone to determine your Master Password by making repeated guesses in a brute force attack. 
+            var stopwatch = Stopwatch.StartNew();
 
-            aes.Key = pbkdf2.GetBytes(32);// set a 256-bit key
-            aes.IV = pbkdf2.GetBytes(16);// set a 128-bit IV (initialization vector) - used to encrypt the first block, so there are no repeating sequences. 
+            var pbkdf2 = new Rfc2898DeriveBytes(
+              password, salt, iterations);
+
+            aes.Key = pbkdf2.GetBytes(32); // set a 256-bit key 
+            aes.IV = pbkdf2.GetBytes(16); // set a 128-bit IV 
+
+            WriteLine("{0:N0} milliseconds to generate Key and IV using {1:N0} iterations.",
+              arg0: stopwatch.ElapsedMilliseconds,
+              arg1: iterations);
 
             using (var ms = new MemoryStream())
             {
-                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                using (var cs = new CryptoStream(
+                  ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
                 {
                     cs.Write(plainBytes, 0, plainBytes.Length);
                 }
                 encryptedBytes = ms.ToArray();
             }
-            return Convert.ToBase64String(encryptedBytes);// Converts it back to a string so it could be inserted into a DB. 
+
+            return Convert.ToBase64String(encryptedBytes);
         }
 
-
-        public static string Decrypt(string cryptoText, string password)
+        public static string Decrypt(
+          string cryptoText, string password)
         {
             byte[] plainBytes;
             byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
 
-            var aes = Aes.Create();//AES stands for Advanced ENcryption Standard, which is todays standard for symmetric encryption.
+            var aes = Aes.Create();
 
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            var pbkdf2 = new Rfc2898DeriveBytes(
+              password, salt, iterations);
 
             aes.Key = pbkdf2.GetBytes(32);
-            aes.IV = pbkdf2.GetBytes(16);// set a 128-bit IV (initialization vector) - used to encrypt the first block, so there are no repeating sequences. 
+            aes.IV = pbkdf2.GetBytes(16);
 
             using (var ms = new MemoryStream())
             {
-                using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                using (var cs = new CryptoStream(
+                  ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
                 {
                     cs.Write(cryptoBytes, 0, cryptoBytes.Length);
                 }
@@ -108,14 +90,167 @@ namespace Packt.Shared
             }
 
             return Encoding.Unicode.GetString(plainBytes);
-
         }
 
-        public class User
+        private static Dictionary<string, User> Users =
+          new Dictionary<string, User>();
+
+        public static User Register(string username, string password,
+          string[] roles = null)
         {
-            public string Name { get; set; }
-            public string Salt { get; set; }
-            public string SaltedHashedPassword { get; set; }
+            // generate a random salt
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+
+            // generate the salted and hashed password 
+            var saltedhashedPassword = SaltAndHashPassword(
+              password, saltText);
+
+            var user = new User
+            {
+                Name = username,
+                Salt = saltText,
+                SaltedHashedPassword = saltedhashedPassword,
+                Roles = roles
+            };
+            Users.Add(user.Name, user);
+
+            return user;
+        }
+
+        // check a user's password that is stored
+        // in the private static dictionary Users
+        public static bool CheckPassword(string username, string password)
+        {
+            if (!Users.ContainsKey(username))
+            {
+                return false;
+            }
+
+            var user = Users[username];
+
+            return CheckPassword(username, password,
+              user.Salt, user.SaltedHashedPassword);
+        }
+
+        // check a user's password using salt and hashed password
+        public static bool CheckPassword(string username, string password,
+          string salt, string hashedPassword)
+        {
+            // re-generate the salted and hashed password 
+            var saltedhashedPassword = SaltAndHashPassword(
+              password, salt);
+
+            return (saltedhashedPassword == hashedPassword);
+        }
+
+        private static string SaltAndHashPassword(
+          string password, string salt)
+        {
+            var sha = SHA256.Create();
+            var saltedPassword = password + salt;
+            return Convert.ToBase64String(
+              sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+        }
+
+        public static string PublicKey;
+
+        public static string ToXmlStringExt(
+          this RSA rsa, bool includePrivateParameters)
+        {
+            var p = rsa.ExportParameters(includePrivateParameters);
+            XElement xml;
+            if (includePrivateParameters)
+            {
+                xml = new XElement("RSAKeyValue",
+                  new XElement("Modulus", ToBase64String(p.Modulus)),
+                  new XElement("Exponent", ToBase64String(p.Exponent)),
+                  new XElement("P", ToBase64String(p.P)),
+                  new XElement("Q", ToBase64String(p.Q)),
+                  new XElement("DP", ToBase64String(p.DP)),
+                  new XElement("DQ", ToBase64String(p.DQ)),
+                  new XElement("InverseQ", ToBase64String(p.InverseQ)));
+            }
+            else
+            {
+                xml = new XElement("RSAKeyValue",
+                  new XElement("Modulus", ToBase64String(p.Modulus)),
+                  new XElement("Exponent", ToBase64String(p.Exponent)));
+            }
+            return xml?.ToString();
+        }
+
+        public static void FromXmlStringExt(
+          this RSA rsa, string parametersAsXml)
+        {
+            var xml = XDocument.Parse(parametersAsXml);
+            var root = xml.Element("RSAKeyValue");
+            var p = new RSAParameters
+            {
+                Modulus = FromBase64String(root.Element("Modulus").Value),
+                Exponent = FromBase64String(root.Element("Exponent").Value)
+            };
+
+            if (root.Element("P") != null)
+            {
+                p.P = FromBase64String(root.Element("P").Value);
+                p.Q = FromBase64String(root.Element("Q").Value);
+                p.DP = FromBase64String(root.Element("DP").Value);
+                p.DQ = FromBase64String(root.Element("DQ").Value);
+                p.InverseQ = FromBase64String(root.Element("InverseQ").Value);
+            }
+            rsa.ImportParameters(p);
+        }
+
+        public static string GenerateSignature(string data)
+        {
+            byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+            var sha = SHA256.Create();
+            var hashedData = sha.ComputeHash(dataBytes);
+
+            var rsa = RSA.Create();
+            PublicKey = rsa.ToXmlStringExt(false); // exclude private key
+
+            return ToBase64String(rsa.SignHash(hashedData,
+              HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
+        }
+
+        public static bool ValidateSignature(
+          string data, string signature)
+        {
+            byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+            var sha = SHA256.Create();
+            var hashedData = sha.ComputeHash(dataBytes);
+            byte[] signatureBytes = FromBase64String(signature);
+            var rsa = RSA.Create();
+            rsa.FromXmlStringExt(PublicKey);
+
+            return rsa.VerifyHash(hashedData, signatureBytes,
+              HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        }
+
+        public static byte[] GetRandomKeyOrIV(int size)
+        {
+            var r = RandomNumberGenerator.Create();
+            var data = new byte[size];
+            r.GetNonZeroBytes(data);
+            // data is an array now filled with 
+            // cryptographically strong random bytes
+            return data;
+        }
+
+        public static void LogIn(string username, string password)
+        {
+            if (CheckPassword(username, password))
+            {
+                var identity = new GenericIdentity(username, "PacktAuth");
+                var principal = new GenericPrincipal(
+                  identity, Users[username].Roles);
+
+                System.Threading.Thread.CurrentPrincipal = principal;
+            }
         }
     }
 }
---
+++ Chapter10/SigningApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+using Packt.Shared;
+
+
+namespace SigningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter some text to sign.");
+            string data = ReadLine();
+
+            var signature = Protector.GenerateSignature(data);
+
+            WriteLine($"Signature: {signature}");
+            WriteLine($"Public Key used to check signature:");
+            WriteLine(Protector.PublicKey);
+
+            if (Protector.ValidateSignature(data, signature))
+            {
+                WriteLine("Correct! Signature is valid.");
+            }
+            else
+            {
+                WriteLine($"Invalid signature.");
+            }
+
+            // simulate a fake signature by replacing the first character with an X
+            var fakeSignature = signature.Replace(signature[0], 'X');
+
+            if (Protector.ValidateSignature(data, fakeSignature))
+            {
+                WriteLine("Correct! Signature is valid.");
+            }
+            else
+            {
+                WriteLine($"Invalid signature: {fakeSignature}");
+            }
+        }
+    }
+}
