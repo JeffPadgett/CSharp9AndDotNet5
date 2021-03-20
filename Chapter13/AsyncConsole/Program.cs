ddf564cb1a3f64ca17f4317db09484f55662e601
Jeffrey Padgett <JeffreyPadgett1@gmail.com>
2021-03-20 18:02:21 -0400

    yield async await asyncEnumerable

---
+++ Chapter13/AsyncConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncConsole
+{
+    class Program
+    {
+        async static Task Main()
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://www.apple.com/");
+
+            WriteLine($"Apple's home page has {response.Content.Headers.ContentLength:N0} bytes.");
+        }
+    }
+}
---
+++ Chapter13/AsyncEnumerable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncEnumerable
+{
+    class Program
+    {
+        async static IAsyncEnumerable<int> GetNumbers()
+        {
+            var r = new Random();
+
+            // simulate work
+            await Task.Run(() => Task.Delay(r.Next(1500, 3000)));
+            yield return r.Next(0, 1001);
+
+            await Task.Run(() => Task.Delay(r.Next(1500, 3000)));
+            yield return r.Next(0,1001);
+
+            await Task.Run(() => Task.Delay(r.Next(1500, 3000)));
+            yield return r.Next(0,1001);
+        }
+        static async Task Main()
+        {
+            await foreach (int number in GetNumbers())
+            {
+                WriteLine($"Number: {number}");
+            }
+        }
+    }
+}
