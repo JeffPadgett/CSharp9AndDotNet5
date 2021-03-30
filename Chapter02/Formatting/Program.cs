a02f06d4eae0cc7c9027e43ea242d6ada33c4fa6
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-03-30 19:55:22 -0400

    Delegates

---
+++ Chapter02/Formatting/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+namespace Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int numberOfApples = 12;
+            // decimal pricePerApple = 0.35M;
+
+            // WriteLine(
+            //     format: "{0} apples cost {1:C}",
+            //     arg0: numberOfApples,
+            //     arg1: pricePerApple * numberOfApples);
+
+            // string formatted = string.Format(format: "{0} apples cost {1:C}",
+            //     arg0: numberOfApples,
+            //     arg1: pricePerApple * numberOfApples
+            //     );
+
+            // //WriteToFile(formatted); //writes the s tring into a file  (Pretend function to just represent an idea)
+            // //Best way
+
+            // string bestFormat = $"{numberOfApples} cost {pricePerApple * numberOfApples:C}";
+            // WriteLine(formatted);
+            // WriteLine(bestFormat);
+
+            Write("press any key combination: ");
+            ConsoleKeyInfo key = ReadKey();
+            WriteLine();
+            WriteLine($"Key: {key.Key}, Char: {key.KeyChar}, Modifiers: {key.Modifiers}");
+        }
+    }
+}
