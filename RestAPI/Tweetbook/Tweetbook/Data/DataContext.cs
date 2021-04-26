b1c071fe8a8877eb874a7434c877d6f64f854ee8
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-04-26 16:09:05 -0400

    first commit - Stripped MVC app to keep authentication and use as an API.

---
+++ RestAPI/Tweetbook/Tweetbook/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tweetbook.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
