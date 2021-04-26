cc51f4a937396c8b367e1beea3f379d691407b33
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-04-26 19:24:40 -0400

    API versioning

---
+++ RestAPI/Tweetbook/Tweetbook/Contracts/ApiRoutes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tweetbook.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        public static class Posts
+        {
+            public const string GetAll = Base + "/posts";
+        }
+    }
+}
---
+++ RestAPI/Tweetbook/Tweetbook/Controllers/V1/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweetbook.Contracts.V1;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Controllers.V1
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _post;
+
+        public PostsController()
+        {
+            _post = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                _post.Add(new Post { Id = Guid.NewGuid().ToString()});
+            }
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_post);
+        }
+     
+    }
+}
