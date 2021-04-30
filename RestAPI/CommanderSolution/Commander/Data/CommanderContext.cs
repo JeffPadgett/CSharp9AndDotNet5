63d74ea5a9ba4fa497544a56127648e0ebaf0702
Jeffrey Padgett <Strife12>
2021-04-29 23:19:44 -0400

    added SQLLite DB. Setup DB context.

---
+++ RestAPI/CommanderSolution/Commander/Data/CommanderContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
--- RestAPI/CommanderSolution/Commander/Startup.cs
+++ RestAPI/CommanderSolution/Commander/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,6 +29,8 @@ namespace Commander
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string sqlLiteDbPath = Path.Combine("..", "CommanderDb.db");
+            services.AddDbContext<CommanderContext>(options => options.UseSqlite($"Data Source={sqlLiteDbPath}"));
 
             services.AddControllers();
 
