3f300b172257c3657aedf2df12928a712dd5572c
jeff <jeffreypadgett1@gmail.com>
2021-05-18 23:58:49 -0400

    * IRestaurantData.cs: * InMemoryRestaurantData.cs: * Detail.cshtml: * NotFound.cshtml: * Detail.cshtml.cs:
    * List.cshtml: Model binding with route and query stirng paramaters

---
+++ OdeToFood/OdeToFoodSolution/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Jeff's Pizza", Location= "Maryland", Cuisine= CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Bowl of Pho", Location= "Jacksonville", Cuisine= CuisineType.Indian},
+                new Restaurant { Id = 3, Name = "Taco Land", Location= "Saint Augustine", Cuisine= CuisineType.Mexican},
+                new Restaurant { Id = 4, Name = "Olive Garden", Location= "Georga", Cuisine= CuisineType.Italian}
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
--- OdeToFood/OdeToFoodSolution/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/OdeToFoodSolution/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -5,18 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
-        
+        private IRestaurantData restaurantData;
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
         public Restaurant Restaurant { get; set; }
 
-        public void OnGet(int restaurantId)
+        public IActionResult OnGet(int restaurantId)
         {
-            Restaurant = new Restaurant();
-            Restaurant.Id = restaurantId;
+            Restaurant = restaurantData.GetById(restaurantId);
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("NotFound");
+            }
+
+            
+            return Page();
         }
     }
 }
